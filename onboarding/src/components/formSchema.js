c1b612886341736a402063a324932b3e05abed0b
jessejohns-1 <84858554+jessejohns-1@users.noreply.github.com>
2021-07-21 19:22:31 -0400

    testing my commits

--- onboarding/src/components/formSchema.js
+++ onboarding/src/components/formSchema.js
@@ -5,4 +5,21 @@ const formSchema = yup.object().shape   ({
     .string()
     .trim()
     .required('Your name is a requirement')
+    .min(4, 'Username must be 4 characters long')
+    .max(10, "Username cannot be more than 10 characters long"),
+    email: yup
+    .string()
+    .email('Must be valid email address')
+    .required('Email is required'),
+    role: yup
+    .string()
+    .oneOf(['instructor', 'student', 'alumni', 'tl'], 'Role is required'),
+    civil: yup
+    .string()
+    .oneOf(['single', 'married'], 'Civil status is required'),
+coding: yup.boolean(),
+reading: yup.boolean(),
+hiking: yup.boolean()
 })
+
+export default formSchema
