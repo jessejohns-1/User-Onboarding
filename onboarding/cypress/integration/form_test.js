349ea4ff17f63826025953de986ee0f52142532e
jessejohns-1 <84858554+jessejohns-1@users.noreply.github.com>
2021-07-22 18:19:11 -0400

    MVP number 2 finished

--- onboarding/cypress/integration/form_test.js
+++ onboarding/cypress/integration/form_test.js
@@ -16,15 +16,20 @@ describe("Users app", () => {
         cy.get('input[name="name"]').type("Putting my words in")
         //checking the value after to validate it added my string
         cy.get('input[name="name"]').should("have.value", "Putting my words in");
-
+        //inputting all my text
         cy.get('input[name="email"]').type('daddykong@gmail.com')
         cy.get('input[name="password"]').type('passworddaddy')
+        //mock checking to see if foobar element is there
         cy.get('input[name="foobar"]').should("not.exist");
-        cy.get('button[id="submitBtn"]')
+        //clicking terms of service
         cy.get('input[name="terms"]').click();
+        //clicking the submit button
         cy.get('button[id="submitBtn"]').click();
+        //checking that the name field has been reset to an empty string
         cy.get('input[name="name"]').should("have.value", "");
+        //unclicking the terms of service button
         cy.get('input[name="terms"]').click();
+        //checking to see if the site contains the error message for terms of service
         cy.contains("you must agree to the terms of service").click
         
     })
